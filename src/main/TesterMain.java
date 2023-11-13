8382494c8ef45be8327d4de0cfee863d16f567e5
diegoq48 <diego.quinones8@upr.edu>
2023-11-13 19:08:49 -0400

    Fixed the tester

--- src/main/TesterMain.java
+++ src/main/TesterMain.java
@@ -16,35 +16,13 @@ public class TesterMain {
             System.out.println(cpf.getMachines().get(6));
             System.out.println(cpf.getMachines().get(2));
             System.out.println(cpf.getPartCatalog().get(1).getName());
-            cpf.runFactory(30, 60);
+            cpf.runFactory(30, 1000);
             cpf.generateReport();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } 
-      /*   CarPart part = new CarPart(9, "Transmission", 15.0, true);
-        PartMachine machine1 = new PartMachine(1, part, 1, 1.2, 5);
-        for (int i = 0; i < 10; i++) {
-            CarPart p2 = machine1.produceCarPart();
-            machine1.printConveyorBelt();
-            if(p2 == null){System.out.println("null");}
-        }
-        part = machine1.produceCarPart();
-        System.out.println("Total Parts Produced: " + machine1.getTotalPartsProduced());
-        System.out.println("Defective Chance: " + machine1.getChanceOfDefective());
-        CarPart part2 = machine1.produceCarPart();
-        System.out.println("Total Parts Produced: " + machine1.getTotalPartsProduced());
-        System.out.println("Defective Chance: " + machine1.getChanceOfDefective());
-        CarPart part3 = machine1.produceCarPart();
-        System.out.println("Total Parts Produced: " + machine1.getTotalPartsProduced());
-        System.out.println("Defective Chance: " + machine1.getChanceOfDefective());
-        System.out.println(part.isDefective());
-        
- */     
-
     }
 
-        //print the total parts produced and defective chance
-
-    }
+}
 
